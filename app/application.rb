8760d568fdb9cf57c9ade836126ad8fc5904bc94
Kayla Reopelle <kreopelle@gmail.com>
2018-07-02 15:29:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,11 @@ class Application
   def greeter
     resp = Rack::Response.new
     binding.pry
-    if Time.now >
+    if Time.now > 12
+      resp.write "Good Afternoon"
+    else 
+      resp.write "Good Morning"
+    end 
 
   end
 
